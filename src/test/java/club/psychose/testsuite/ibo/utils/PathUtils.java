d829d9575539cec0eb3c967e096568d3d2d08a90
CrashedLife <33805688+CrashedLife@users.noreply.github.com>
2023-01-30 10:23:03 +0100

    Added PathUtils.java

---
+++ src/test/java/club/psychose/testsuite/ibo/utils/PathUtils.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2023, psychose.club
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package club.psychose.testsuite.ibo.utils;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public final class PathUtils {
+    // Folder constant.
+    public static Path getTestSuiteFolderPath (String additionalPath) {
+        return additionalPath != null ? getOSPath(Paths.get(System.getProperty("user.home") + "\\psychose.club\\TestSuites\\IBO\\" + additionalPath)) : getOSPath(Paths.get(System.getProperty("user.home") + "\\psychose.club\\TestSuites\\IBO\\"));
+    }
+
+    // This returns a path with the valid file separators for the OS.
+    public static Path getOSPath (Path path) {
+        return Paths.get(path.toString().trim().replace("\\", File.separator));
+    }
+}
