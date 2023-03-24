e89c7df1af270e4aaca489930db0f9ec7d0f03c2
söri <33805688+CrashedLife@users.noreply.github.com>
2023-03-24 17:28:47 +0100

    [TestSuite] Added the new TestCases to the IBOTestSuite.java

--- src/test/java/club/psychose/testsuite/ibo/IBOTestSuite.java
+++ src/test/java/club/psychose/testsuite/ibo/IBOTestSuite.java
@@ -30,8 +30,7 @@
 package club.psychose.testsuite.ibo;
 
 import club.psychose.testsuite.ibo.testcases.Test;
-import club.psychose.testsuite.ibo.testcases.reader.TC0011ReaderMemoryBinaryReader;
-import club.psychose.testsuite.ibo.testcases.reader.TC0012ReaderFileBinaryReader;
+import club.psychose.testsuite.ibo.testcases.reader.*;
 import club.psychose.testsuite.ibo.testcases.structures.*;
 import club.psychose.testsuite.ibo.testcases.writer.TC0009WriterBinaryWriterNoChunkPadding;
 import club.psychose.testsuite.ibo.testcases.writer.TC0010WriterBinaryWriterChunkPadding;
@@ -61,6 +60,9 @@ public final class IBOTestSuite {
         testCasesArrayList.add(new TC0010WriterBinaryWriterChunkPadding()); // BinaryWriter with chunk padding.
         testCasesArrayList.add(new TC0011ReaderMemoryBinaryReader()); // MemoryBinaryReader
         testCasesArrayList.add(new TC0012ReaderFileBinaryReader()); // FileBinaryReader
+        testCasesArrayList.add(new TC0013ReaderPaddingRemoval()); // Removing the padding from bytes.
+        testCasesArrayList.add(new TC0014ReaderHEXValueSearchMemoryBinaryReader()); // Searches for a HEX value in the memory.
+        testCasesArrayList.add(new TC0015ReaderHEXValueSearchFileBinaryReader()); // Searches for a HEX value in a file.
     }
 
     private static void runTests () {
