4fa28d5950367d3606080eacc55e4d351368b2b9
söri <33805688+CrashedLife@users.noreply.github.com>
2023-11-18 19:35:50 +0100

    Added IBOConstants.java

---
+++ src/main/java/club/psychose/library/ibo/utils/IBOConstants.java
@@ -0,0 +1,8 @@
+package club.psychose.library.ibo.utils;
+
+/**
+ * This class contains the constant for this library.
+ */
+public final class IBOConstants {
+    public static final String LIBRARY_VERSION = "2.2.0";
+}
